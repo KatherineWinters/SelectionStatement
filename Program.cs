cb3bb3bc99bb65522a53396fa6867698c7245ac6
Katherine Winters <goble.katherine@gmail.com>
2021-12-07 16:11:15 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SelectionStatementsExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            int favoriteNumber = 100;
+
+            Console.Write("Guess my favorite number: ");
+            int guess = int.Parse(Console.ReadLine());
+
+            if (guess < favoriteNumber)
+            {
+                Console.WriteLine("too low");
+            }
+            else if (guess > favoriteNumber)
+            {
+                Console.WriteLine("too high");
+            }
+            else
+            {
+                Console.WriteLine("Correct");
+            }
+        }
+    }
+}
